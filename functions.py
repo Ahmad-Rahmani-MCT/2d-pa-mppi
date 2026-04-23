ed6e8736ee683d4cfcd4fbbecdc60b1dc4b53e59
Ahmad Rahmani <s320209@studenti.polito.it>
2026-04-23 12:24:46 +0200

    added the vanilla mppi

--- functions.py
+++ functions.py
@@ -94,4 +94,108 @@ def dynamics_step(state, control, dt, max_speed) :
     # repacking the state 
     next_state = jnp.stack([next_px, next_py, next_theta, next_v], axis=-1) 
 
-    return next_state
+    return next_state 
+
+def single_trajectory_rollout(state_init, control_sequence, dt): 
+    """
+    simulate a single trajectory over the control horizon steps
+    """
+    def step_fn(current_state, current_control): 
+        # update the state 
+        next_state = dynamics_step(current_state, current_control) 
+        # jax.lax.scan requires returning the 'carry' (state to pass to next step) 
+        # and the 'output' (what we want to save in the final array) 
+        return next_state, next_state 
+    
+    # jax.lax.scan loops over the control_sequence instead of a for loop 
+    # it returns the very final state, and an array of all intermediate states (the path) 
+    _, path = jax.lax.scan(step_fn, state_init, control_sequence) 
+
+    return path 
+
+# magic of vmap 
+# no need to map the initial state (all trajectories start from the same initial state) 
+# but map the first axis of the control array (ofcourse) 
+# so we can roll N parallel trajectories simultaenously 
+batch_rollout = jax.vmap(single_trajectory_rollout, in_axes=(None, 0, None)) 
+
+@jax.jit
+def mppi_step(state, nominal_controls, belief_map, goal_pose, prng_key, N=1000, H=15, dt=0.1, lam=0.02, resolution=0.1): 
+    """
+    performs one complete mppi optimization 
+    # state: current drone state (4,)  [px, py, theta, v] 
+    # nominal_controls: best control sequence from the last step (H, 2) 
+    # belief_map: drone 2D map knowledge 
+    # goal_pos: target [x, y] coordinates 
+    # prng_key: JAX random key 
+    # N: number of samples 
+    # H: horizon length 
+    # lam: lambda (mppi temperature parameter)
+    """
+    # sampling (JAX procedure) 
+    key, subkey = jax.random.split(prng_key) 
+
+    # generate gassian noise for N trajectories, h steps and 2 control inputs 
+    # assuming a std dev of 1.0 for acceleration and 0.5 for omega 
+    noise_std = jnp.array([1.0, 0.5]) 
+    noise = jax.random.normal(subkey, shape=(N, H, 2)) * noise_std 
+
+    # apply noise to the nominal control to create N different control sequences 
+    # nominal_controls shape (H, 2) but noise is (N, H, 2) JAX broadcasts this 
+    perturbed_controls = nominal_controls + noise 
+
+    # clipping the controls 
+    # perturbed_controls = jnp.clip(perturbed_controls, min_control_bounds, max_control_bounds) 
+
+    # rollouts  
+    # using of vmap function to simulate all N trajectories in parallel 
+    # paths will be of shape (N, H, 4) 
+    paths = batch_rollout(state, perturbed_controls, dt) 
+
+    # cost evaluation 
+    # extracting x and y positions from the paths 
+    px = paths[..., 0] 
+    py = paths[..., 1] 
+
+    # distance to goal at the final step 
+    # paths[:, -1, :2] gives the final [px, py] for all N trajectories 
+    final_positions = paths[..., -1, :2] 
+    goal_cost = jnp.sum((final_positions-goal_pose)**2, axis=-1) * 2.5 # weighting factor 
+
+    # collision cost  
+    # converting continuous positions to matrix indices 
+    col_indices = jnp.floor(px/resolution).astype(jnp.int32) 
+    row_indices = jnp.floor(py/resolution).astype(jnp.int32) 
+
+    # clip indices in case drone flies out of the map 
+    max_rows, max_cols = belief_map.shape 
+    col_indices = jnp.clip(col_indices, 0, max_cols-1) 
+    row_indices = jnp.clip(row_indices, 0, max_rows - 1) 
+
+    # looking for the map values for every point (N, H) 
+    map_values = belief_map[row_indices, col_indices] 
+
+    # massive penalty for for when the visited space is not free (0) 
+    collision_cost = jnp.sum(map_values != 0, axis=-1) * 10000.0 
+
+    # total cost (shape: N) 
+    total_cost = goal_cost + collision_cost 
+
+    # weight update 
+    # subtract the minimum cost for numerical stability (prevents e^-infinity = 0) 
+    beta = jnp.min(total_cost) 
+
+    # calculate exponential weights: w = exp( -(S - min(S)) / lambda ) 
+    weights = jnp.exp(- (total_cost - beta) / lam) 
+
+    # normalize weights so they sum to 1 
+    weights = weights / jnp.sum(weights) 
+
+    # new optimal control sequence by doing a weighted sum of the noise 
+    # weights is (N,), noise is (N, H, 2) 
+    # reshape weights to (N, 1, 1) so it broadcasts over H and the 2 control inputs 
+    optimal_control_sequence = nominal_controls + jnp.sum(weights.reshape(N, 1, 1) * noise, axis=0) 
+
+    # new nominal controls to be used next timestep 
+    # and new PRNG key so we can generate different random numbers next time 
+    return optimal_control_sequence, key 
